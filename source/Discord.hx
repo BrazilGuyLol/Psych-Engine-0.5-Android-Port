a7abfbd579fa73f96803aeb84f1700bbe31e7e61
BrazilGuyLol <96142970+BrazilGuyLol@users.noreply.github.com>
2022-03-14 20:30:04 -0300

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -42,10 +42,10 @@ class DiscordClient
 	static function onReady()
 	{
 		DiscordRpc.presence({
-			details: "In the Menus",
+			details: "Corrupting the Menus...",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Corrupted Engine"
 		});
 	}
 
@@ -81,7 +81,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Corrupted Engine Version: " + MainMenuState.psychEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
